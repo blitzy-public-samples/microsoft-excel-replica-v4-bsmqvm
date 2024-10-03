769730c688f858c6b29d19a445ea4bf972c748d5
Siddhant P. Pardeshi <siddhantpp@gmail.com>
2024-10-03 16:39:39 +0530

    Add public-samples/microsoft-excel-replica-v4-bsmqvm/src/frontend-desktop/ExcelDesktop/Properties/AssemblyInfo.cs

---
+++ src/frontend-desktop/ExcelDesktop/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ExcelDesktop")]
+[assembly: AssemblyDescription("Desktop application for Microsoft Excel")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("Microsoft Excel")]
+[assembly: AssemblyCopyright("Copyright © Microsoft Corporation 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a1b2c3d4-e5f6-47g8-h9i0-j1k2l3m4n5o6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("16.0.15000.20000")]
+[assembly: AssemblyFileVersion("16.0.15000.20000")]
